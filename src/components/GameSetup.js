b521de9fb9fece14d80f727f5b7ae913ac6bd38d
Drew Miller <drew@onpatrol.co>
2016-08-20 09:35:56 +0100

    Add a button to validate game setup

--- src/components/GameSetup.js
+++ src/components/GameSetup.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Button from 'react-native-button';
 
 import styles from '../styles/main';
 
@@ -12,12 +13,23 @@ export default class GameSetup extends Component {
         super(props);
     }
 
+    validateSetup () {
+        console.log('validating setup');
+        return false;
+    }
+
     render () {
         return (
             <View style={ styles.page }>
-                <Text style={ styles.heading }>Player Entry</Text>
+                <Text style={ styles.heading }>Game Setup</Text>
                 <GameSetup.SetGoal goal={this.props.goal} updateGoal={this.props.updateGoal} />
                 <GameSetup.EnterPlayers addPlayer={this.props.addPlayer} removePlayer={this.props.removePlayer} players={this.props.players} />
+                <Button
+                    containerStyle={{padding: 10, height: 45, overflow: 'hidden', borderRadius: 4, backgroundColor: 'blue' }}
+                    style={{fontSize: 20, color: 'white'}}
+                    onPress={ this.validateSetup.bind(this) }>
+                    Start Game
+                </Button>
             </View>
         );
     }
